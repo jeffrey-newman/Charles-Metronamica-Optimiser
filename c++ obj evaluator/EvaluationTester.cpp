0df5b9773ddb408df225297bf4662e5f1b68fca6
Jeffrey Newman <jeffrey.newman.au@gmail.com>
2016-07-20 12:55:40 +0930

    evaltester now using new OF

--- c++ obj evaluator/EvaluationTester.cpp
+++ c++ obj evaluator/EvaluationTester.cpp
@@ -6,7 +6,7 @@
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/foreach.hpp>
-#include "MetronamicaOF.hpp"
+#include "MetronamicaOF2.hpp"
 #include "Pathify.hpp"
 
 
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
     pathify(wine_reg_mod_file);
     pathify(log_file);
 
-    MetronamicaOF metro_eval(metro_exe.second,
+    MetronamicaOF2 metro_eval(metro_exe.second,
                              mck_exe.second,
                              wine_exe.second,
                              java_exe.second,
